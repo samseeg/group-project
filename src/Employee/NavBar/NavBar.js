cd47c9c8ea558229b3d3740cc5f7291a44c79ac8
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-11-15 14:09:57 -0700

    navbar changes

--- src/Employee/NavBar/NavBar.js
+++ src/Employee/NavBar/NavBar.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import BadgeExampleSimple from './../Notification/Notification'
 import './NavBar.css';
 
 class NavBar extends Component {
+
+    constructor(){
+        super();
+        
+        this.state = {
+            menuOpen: false
+        }
+
+        this.slide = this.slide.bind(this);
+    }
+
+    slide() {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        })
+    }
+
     render() {
         return (
             <div className='NavBar'>
-                <div>
-                    <img src='https://i.imgur.com/D6FrXyC.png' alt='' />
+                <div className='notification'>
+                <img src='https://i.imgur.com/Kky6XPi.png' alt='' />
                 </div>
-                <div>
-                    <img src='https://i.imgur.com/e65SwpT.png' alt='' />
+                <div className='hamburger' onClick={this.slide}>
+                    <img src='https://i.imgur.com/R7uPgPM.png' alt='' />
                 </div>
-                <div className='menu'>
-                    <ul>
+                <div className={this.state.menuOpen ? "slide open" : "slide"}>                    
                         <Link className="active" to="/empmain">CLOCK IN/OUT</Link>
                         <Link className="active" to="/timecard">TIME CARD</Link>
                         <Link className="active" to="/vacarequests">TIME OFF REQUESTS</Link>
                         <Link className="active" to="/calendar">CALENDAR</Link>
-                        <Link className="active" to="/">LOGOUT</Link>
-                    </ul>
+                        <Link className="active" to="/">LOGOUT</Link>                    
                 </div>
             </div>
         )
