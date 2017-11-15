f747298c703ac6e183575f45bbc1ec043030caf3
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-11-15 15:35:33 -0700

    notification menu working

--- src/Employee/NavBar/NavBar.js
+++ src/Employee/NavBar/NavBar.js
@@ -9,34 +9,50 @@ class NavBar extends Component {
         super();
 
         this.state = {
-            menuOpen: false
+            menuOpen: false,
+            notificationsOpen: false
         }
 
-        this.slide = this.slide.bind(this);
+        this.menuSlide = this.menuSlide.bind(this);
+        this.notificationsSlide = this.notificationsSlide.bind(this);
     }
 
-    slide() {
+    menuSlide() {
         this.setState({
             menuOpen: !this.state.menuOpen
+        })
+    }
+
+    notificationsSlide() {
+        this.setState({
+            notificationsOpen: !this.state.notificationsOpen
         })
     }
 
     render() {
         return (
             <div className='NavBar'>
-                <div className='notification'>
+                <div className='notification' onClick={this.notificationsSlide}>
                     <img src='https://i.imgur.com/Kky6XPi.png' alt='' />
                 </div>
-                <div className='hamburger' onClick={this.slide}>
+
+                <div className='hamburger' onClick={this.menuSlide}>
                     <img src='https://i.imgur.com/R7uPgPM.png' alt='' />
                 </div>
-                <div className={this.state.menuOpen ? "open" : "hide"}>
-                    <img src='https://i.imgur.com/bFsllTF.png' alt='' onClick={this.slide} />
+                <div className={this.state.menuOpen ? "menuOpen" : "menuHide"}>
+                    <img src='https://i.imgur.com/bFsllTF.png' alt='' onClick={this.menuSlide} />
                     <Link className="active" to="/empmain">CLOCK IN / OUT</Link>
                     <Link className="active" to="/timecard">TIME CARD</Link>
                     <Link className="active" to="/vacarequests">TIME OFF REQUESTS</Link>
                     <Link className="active" to="/calendar">CALENDAR</Link>
                     <Link className="active" to="/">LOGOUT</Link>
+                </div>
+                <div className={this.state.notificationsOpen ? "notificationsOpen" : "notificationsHide"}>
+                    <img src='https://i.imgur.com/bFsllTF.png' alt='' onClick={this.notificationsSlide} />
+                    <div className='mapped_notifications'>
+                        {/* mapped notifications go here */}
+                        mapped notifications go here
+                    </div>
                 </div>
             </div>
         )
