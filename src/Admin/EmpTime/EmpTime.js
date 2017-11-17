711875103ef471401ec95e36e181d1c536010a88
eunbink <kangeb1992@gmail.com>
2017-11-17 14:15:49 -0700

    employee timecard working

--- src/Admin/EmpTime/EmpTime.js
+++ src/Admin/EmpTime/EmpTime.js
@@ -1,27 +1,67 @@
 import React, { Component } from 'react';
 import './EmpTime.css';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+  } from 'material-ui/Table';
+  import axios from 'axios';
 import NavBar from '../../Employee/NavBar/NavBar.js';
 
 class EmpTime extends Component {
     constructor() {
         super()
-    }
-
+        this.state = {
+          timecard: []
+        }
+      }
+      componentDidMount() {
+        axios.get('/api/employee/get_timecard').then(response => {
+          this.setState({
+            timecard: response.data
+          })
+          console.log(response.data)
+        })
+      }
 
     render() {
+        const EmptimecardDisplayed = this.state.timecard.map((timecard, i) => {
+          return (
+           
+              <TableRow>
+                <TableRowColumn>{timecard.clock_in}</TableRowColumn>
+                <TableRowColumn>{timecard.clock_out}</TableRowColumn>
+                <TableRowColumn>{timecard.total_hours}</TableRowColumn>
+              </TableRow>
+          )
+        })
+        
         return (
-            <div>
-                <NavBar />
-                <div>
-                    <h1>Time Card</h1>
-
-                    <div>
-                        Employee pay period info
-                    </div>
-                </div>
+          <div>
+            <NavBar />
+            <div className='timecard_container'>
+              <div className='whitespace'></div>
+              <Table>
+                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                  <TableRow>
+                    <TableHeaderColumn colSpan="3">TIMECARD</TableHeaderColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableHeaderColumn>Clock In</TableHeaderColumn>
+                    <TableHeaderColumn>Clock Out</TableHeaderColumn>
+                    <TableHeaderColumn>Total Hours</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                  {EmptimecardDisplayed}
+                </TableBody>
+              </Table>
             </div>
+          </div>
         )
+      }
     }
-}
-
 export default EmpTime;
