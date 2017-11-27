4b32cf2ff10b4a72ad0cdd259ab1320791fcf9e1
eunbink <kangeb1992@gmail.com>
2017-11-27 14:05:40 -0700

    nav

--- src/Employee/Notification/Notification.js
+++ src/Employee/Notification/Notification.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Badge from 'material-ui/Badge';
 import IconButton from 'material-ui/IconButton';
 import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 import EmpRequest from './../../Admin/EmpRequest/EmpRequest.js';
+import axios from 'axios';
 
 
-// class Notification extends Component {
-//   constructor() {
-//     super();
+class Notification extends Component {
+  constructor() {
+    super();
+    this.state = {
+      requests: []
+    }
+  }
+  componentDidMount() {
+    axios.get('/api/admin/get_requests').then(response => {
+        this.setState({
+            requests: response.data
+        })
+    })
+}
+  render() {
+    const notificationDisplayed = this.state.requests.map((requests, i) => {
+      return (
+        <div>
+          <div>
+          {requests.start_date}
+          {requests.end_date}
+          {requests.approval}
+          </div>
+        </div>
 
-//     this.state = {
-      
-//     }
-//   }
-// }
-const BadgeExampleSimple = () => (
-  <div>
-    <Badge
-      badgeContent={10}
-      secondary={true}
-      badgeStyle={{top: 12, right: 12}}
-    >
-      <IconButton tooltip="Notifications">
-        <NotificationsIcon />
-      
-      </IconButton>
-    </Badge>
-  </div>
-);
+      )
+    })
+    
+    return (
+      <div>
+        {notificationDisplayed }
+        </div>
+    )
+  }
+}
 
 
 
-export default BadgeExampleSimple;
+
+
+export default Notification;
 
