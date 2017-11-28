cc7d0ccb09952f2c5327a1deb3ed6d2a14eda994
SarahJay55 <30481405+SarahJay55@users.noreply.github.com>
2017-11-28 12:12:00 -0700

    delete request from admin

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const initialState = {
-    user: {}
+    user: {},
+    request: []
 }
 
 const GET_USER_INFO = "GET_USER_INFO";
+const REMOVE_REQUEST = "REMOVE_REQUEST";
 
 export function getUserInfo() {
     const userData = axios.get('/auth/me')
@@ -17,10 +19,21 @@ export function getUserInfo() {
     }
 }
 
+export function removeRequest(request){
+    return {
+        type: REMOVE_REQUEST,
+        payload: request
+    }
+}
+
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case GET_USER_INFO + '_FULFILLED':
             return Object.assign({}, state, { user: action.payload })
+        case REMOVE_REQUEST + '_FULFILLED':
+            let newArray = state.request.slice();
+            newArray.splice(action.payload, 1);
+            return Object.assign({}, state, { request: newArray })
         default:
             return state;
     }
