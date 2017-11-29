be0e9acdae7d53771536c5a49a0f359be87e8304
sambseeg <samseegmiller@gmail.com>
2017-11-29 09:11:24 -0700

    fixed order of get requests

--- db/get_admin_requests.sql
+++ db/get_admin_requests.sql
@@ -1,2 +1,3 @@
 select * from users
 join requests on users.id = requests.user_id
+order by requests.id desc
--- db/get_timecard.sql
+++ db/get_timecard.sql
@@ -1,3 +1,4 @@
 select * from users
 join timecard on users.id = timecard.user_id
 where users.id = $1
+order by timecard.id desc
