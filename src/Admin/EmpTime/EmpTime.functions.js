6fd3a577729bcef500e2399d7f71e0032ef89fb4
eunbink <kangeb1992@gmail.com>
2017-12-04 13:54:42 -0700

    nodemon working

--- src/Admin/EmpTime/EmpTime.functions.js
+++ src/Admin/EmpTime/EmpTime.functions.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = {
     getEmpTimeCard(url) {
         console.log("hey")
-        return axios.get('http://localhost:3005/api/admin/get_emp_timecard/Eunbin%20Kang').then( response => {
+        return axios.get(url).then( response => {
             return response.data;
         })
     }
